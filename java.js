a3306a4ccede7398340fa93a2d1b60e4a19037a7
vivek sethaliya <63964815+viveksethaliya@users.noreply.github.com>
2020-04-19 20:39:34 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,47 @@
+
+  function val_txt() 
+  { 
+   var x = document.getElementById('ai').value; 
+   if (x === "go") 
+   { 
+    alert ("where");
+    return false;
+   }
+if (x === "who is your owner") 
+   { 
+    alert ("vivek sethaliya");
+    return false;
+   }
+if (x === "how are you") 
+   { 
+    alert ("i am fine");
+    return false;
+   }
+var x = document.getElementById('ai').value; 
+   if (x === "hi") 
+   { 
+    alert ("hello!");
+    return false;
+   }
+if (x === "what is your name") 
+   { 
+    alert ("my name is bapuji");
+    return false;
+   }
+var z = document.getElementById('ai').value;
+if (z === "how are you") 
+   { 
+    alert ("i am fine");
+    return false;
+   }
+if (z === "what is your problem") 
+   { 
+    alert ("i have no idea");
+    return false;
+   }
+if (z === "fuck") 
+   { 
+    alert ("Hayy please don't abuse");
+    return false;
+   }
+}
